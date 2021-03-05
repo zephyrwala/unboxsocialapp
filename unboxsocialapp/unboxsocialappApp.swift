4286d468abb23852c8d4848c2359f5de0e34732e
zephyrwala <mayur@redphoenix.co.in>
2021-03-05 16:30:13 +0530

    Initial Commit

---
+++ unboxsocialapp/unboxsocialappApp.swift
@@ -0,0 +1,17 @@
+//
+//  unboxsocialappApp.swift
+//  unboxsocialapp
+//
+//  Created by Red Phoenix on 05/03/21.
+//
+
+import SwiftUI
+
+@main
+struct unboxsocialappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
